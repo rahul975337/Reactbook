85ac3887bec7962eee6316691df1323faf0c1309
rahul975337 <tilwani03@gmail.com>
2020-12-01 01:16:04 +0530

    left sidebar done

---
+++ src/components/Sidebar/SidebarRow.js
@@ -0,0 +1,14 @@
+import { Avatar, Icon } from "@material-ui/core";
+import React from "react";
+import "./SidebarRow.css";
+function SidebarRow({ title, Icon, src }) {
+  return (
+    <div className="sidebarRow">
+      {src && <Avatar src={src} />}
+      {Icon && <Icon />}
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+export default SidebarRow;
