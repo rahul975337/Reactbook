7cd3cf17165ba75370521a1fa5fc8b6bc0703737
rahul975337 <tilwani03@gmail.com>
2020-11-30 22:44:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* H E A D E R */}
+      <Header />
+      <div className="app__body">
+        {/* S I D E B A R */}
+        <Sidebar />
+        {/* F E E D  */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTMKQEmwz_NqQ-wkG-B3N7g6P7iDE1Qts",
+  authDomain: "reactbook-be109.firebaseapp.com",
+  databaseURL: "https://reactbook-be109.firebaseio.com",
+  projectId: "reactbook-be109",
+  storageBucket: "reactbook-be109.appspot.com",
+  messagingSenderId: "334853803046",
+  appId: "1:334853803046:web:a72a311a8bd1c059f3eae7",
+  measurementId: "G-LEQWPR818W",
+};
